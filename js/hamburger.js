ddde240bae3551144538e27a86a40d7311e95601
Sreeroop S K <sreeroopsk10@gmail.com>
2020-05-29 11:58:07 +0530

    hamburger bug not fixed,but uploading without animation

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,21 +1,21 @@
 const navSlide = () => {
-  const burger = document.querySelector(".burger");
-  const nav = document.querySelector(".navLinks");
-  const navLinks = document.querySelectorAll(".navLinks li");
+  const burger = document.querySelector(`.burger`);
+  const nav = document.querySelector(`.navLinks`);
+  const navLinks = document.querySelector(`.navLinks li`);
 
-  burger.addEventListener("click", () => {
-    nav.classList.toggle("navActive");
+  burger.addEventListener(`click`, () => {
+    nav.classList.toggle(`nav-active`);
 
     navLinks.forEach((link, index) => {
       if (link.style.animation) {
-        link.style.animation = "";
+        link.style.animation = '';
       } else {
         link.style.animation = `navAnim 0.5s ease forward ${index / 7 + 2}s`;
       }
     });
-    burger.classList.toggle('toggle');
-  });
 
-  // burger.classList.toggle('toggle');
+    burger.classList.toggle(`toggle`);
+
+  });
 };
 navSlide();
