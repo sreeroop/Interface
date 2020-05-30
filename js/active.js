ca1c7bab673b37a8190c1073c141e705b9191de6
Sreeroop S K <sreeroopsk10@gmail.com>
2020-05-30 10:39:50 +0530

    map added and footer added,active.js not working

---
+++ js/active.js
@@ -0,0 +1,11 @@
+var header = document.getElementsByClassName('navLinks');
+var btns = header.getElementsByClassName('links');
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName('active');
+        if (current.length > 0) {
+            current[0].className = current[0].className.replace('active', '');
+        }
+        this.className += 'active';
+    });
+}
