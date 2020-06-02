3e19079bce4f026240f583b3be803312244ec7a9
Sreeroop S K <sreeroopsk10@gmail.com>
2020-06-03 01:44:00 +0530

    main pic added

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,16 +1,16 @@
 const navSlide = () => {
-  const burger = document.querySelector(`.burger`);
-  const nav = document.querySelector(`.navLinks`);
-  const navLinks = document.querySelector(`.navLinks li`);
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".navLinks");
+  const navLinks = document.querySelector(".navLinks li");
 
-  burger.addEventListener(`click`, () => {
-    nav.classList.toggle(`nav-active`);
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
 
     navLinks.forEach((link, index) => {
       if (link.style.animation) {
         link.style.animation = '';
       } else {
-        link.style.animation = `navAnim 0.5s ease forward ${index / 7 + 2}s`;
+        link.style.animation = `navAnim 5s ease forward ${index / 7 + 5}s`;
       }
     });
 
