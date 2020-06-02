844ba2a2bc9c5cbd648662684596dc9ffe3795c7
Sreeroop S K <sreeroopsk10@gmail.com>
2020-06-02 21:05:02 +0530

    card animation added

--- js/active.js
+++ js/active.js
@@ -1,11 +1,31 @@
-var header = document.getElementsByClassName('navLinks');
-var btns = header.getElementsByClassName('links');
-for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function () {
-        var current = document.getElementsByClassName('active');
-        if (current.length > 0) {
-            current[0].className = current[0].className.replace('active', '');
-        }
-        this.className += 'active';
-    });
-}
+var sections = $('section')
+  , nav = $('li')
+  , nav_height = nav.outerHeight();
+
+$(window).on('scroll', function () {
+  var cur_pos = $(this).scrollTop();
+  
+  sections.each(function() {
+    var top = $(this).offset().top - nav_height,
+        bottom = top + $(this).outerHeight();
+    
+    if (cur_pos >= top && cur_pos <= bottom) {
+      nav.find('a').removeClass('active');
+      sections.removeClass('active');
+      
+      $(this).addClass('active');
+      nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+    }
+  });
+});
+
+nav.find('a').on('click', function () {
+  var $el = $(this)
+    , id = $el.attr('href');
+  
+  $('html, body').animate({
+    scrollTop: $(id).offset().top - nav_height
+  }, 500);
+  
+  return false;
+});
