3d890e04fc8864e9b30187032385e31f1e4a79b6
Sreeroop S K <sreeroopsk10@gmail.com>
2020-06-06 01:37:37 +0530

    some components made responsive

--- js/active.js
+++ js/active.js
@@ -1,31 +1,15 @@
-var sections = $('section')
-  , nav = $('li')
-  , nav_height = nav.outerHeight();
+$(document).ready(function () {
 
-$(window).on('scroll', function () {
-  var cur_pos = $(this).scrollTop();
-  
-  sections.each(function() {
-    var top = $(this).offset().top - nav_height,
-        bottom = top + $(this).outerHeight();
-    
-    if (cur_pos >= top && cur_pos <= bottom) {
-      nav.find('a').removeClass('active');
-      sections.removeClass('active');
-      
-      $(this).addClass('active');
-      nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
-    }
+  $('.first-button').on('click', function () {
+
+    $('.animated-icon1').toggleClass('open');
   });
-});
+  $('.second-button').on('click', function () {
 
-nav.find('a').on('click', function () {
-  var $el = $(this)
-    , id = $el.attr('href');
-  
-  $('html, body').animate({
-    scrollTop: $(id).offset().top - nav_height
-  }, 500);
-  
-  return false;
+    $('.animated-icon2').toggleClass('open');
+  });
+  $('.third-button').on('click', function () {
+
+    $('.animated-icon3').toggleClass('open');
+  });
 });
